d8788356875d5b4b5a27af7aae779096eefbe580
Clanky7 <avihayv@post.bgu.ac.il>
2025-08-31 20:13:13 +0300

    added a ui and some basic functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
+const frame =  document.querySelector(".grid-section");
+const sizes = [4,8,16];
+let counter = 0;
+
 function createSquare(squareSize){
-    const frame =  document.querySelector(".frame");
     const square = document.createElement('div');
     square.classList.add("square")
     square.style.width = squareSize;
@@ -7,23 +10,31 @@ function createSquare(squareSize){
     frame.appendChild(square);
 }
 
-function createGrid(e){
-    const frame =  document.querySelector(".frame");
-   const numOfSquares = e.target.innerText;
+function black(){
+    const squares = document.querySelectorAll(".grid-section div");
+    squares.forEach(square => square.addEventListener('mouseover',() => {
+        square.style.backgroundColor = "black";
+    }));
+}
+
+function createGrid(){
+    frame.innerHTML = "";
+    const numOfSquares = sizes[counter];
     squareWidth = 100/numOfSquares + "%";
     console.log(frame.clientWidth);
     console.log(squareWidth);
-    
-    
-    
-
     for (let i = 0; i < numOfSquares*numOfSquares;i++){ 
         createSquare(squareWidth);
-
     } 
+    counter = (counter + 1) % sizes.length;
 }
 
-const btns = document.querySelectorAll("button");
+document.querySelector(".size button").addEventListener('click',createGrid);
+document.querySelector(".black button").addEventListener('click',black);
 
-btns.forEach(btn => btn.addEventListener('click',createGrid));
+createGrid();
+
+
+
+
 
